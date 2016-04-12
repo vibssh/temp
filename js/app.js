0c8bc97d080d20471e8c130e38db303342d94515
Leo Jacobs <leo@the-earthworks.com>
2016-04-12 14:19:30 +0100

    js changed for accordian to smooth slidedown and slideup

--- js/app.js
+++ js/app.js
@@ -2,20 +2,40 @@
  * Created by LJacobs on 4/11/2016.
  */
 
-/*global window, console, $, jquery*/
+
 $(function () {
-    var item = $('.accordian-item'),
-        content = $('.accordian-content');
+    var $items = $('.accordian-item'),
+        $contents = $(".accordian-content");
 
     //click event
-    $(item).on("click", function(e) {
-        e.preventDefault();
-        var id = $(this).attr('data-id');
-        $(content).each(function(){
-            $(this).slideUp();
-            if($(this).attr('data-id') == id) {
-                $(this).slideDown();
+    $items.on("click", function () {
+        var $clickedItem = $(this);
+        var id = $clickedItem.attr('data-id');
+        var content = $(".accordian-content[data-id='" +id + "']");
+        if (content.is(":visible")) {
+            content.slideUp();
+        } else {
+            $contents.slideUp().promise().done(function () {
+                content.slideDown();
+            });
+        }
+
+        /*
+        $(content).each(function () {
+            if($(this).is(""))
+            //$(this).slideUp();
+            if($(this).attr('data-id') === id) {
+                if ($(this).is(":visible")) {
+                    setTimeout(function() {
+                        $(this).slideDown();
+                    }, 2000);
+                } else {
+                    $(this).slideDown();
+                }
+                    
+               
+                
             }
-        });
+        });*/
     });
 });
